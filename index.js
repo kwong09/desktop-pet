c9bbc0cf99b16b1724cfd926bfe134ac2f5fe55e
kwong09 <wongkayla09@gmail.com>
2025-12-11 17:04:13 -0800

    electron js setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 600,
+        height: 600,
+    })
+
+    win.loadFile('index.html')
+    win.setMenuBarVisibility(false)
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
